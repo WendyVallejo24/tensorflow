94532c35de3b01e45f04caf3ed4862bb432a9ac4
Wendy Vallejo <wbvalpat24@gmail.com>
2023-07-20 13:18:58 -0600

    tensorflow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import WorkflowButton from './WorkFlowButton';
+import TensorFlow from './TensorFlow';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Disparar WorkFlow</h1>
+      <WorkflowButton />
+      <h2>TensorFlow</h2>
+      <TensorFlow />
     </div>
   );
 }
